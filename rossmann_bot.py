ac9124a2c3780d5e5f33a23c761a3021bd0553a1
biazotogabriel <biazotogabriel@gmail.com>
2023-03-27 11:00:21 -0300

    ajsutando porta

--- rossmann_bot.py
+++ rossmann_bot.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import json
 import requests
@@ -86,6 +87,7 @@ def index():
         return '<h1>Rossmann Telegram BOT</h1>'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = os.environ.get('PORT', 5000)
+    app.run(host='0.0.0.0', port=port)
 
 
